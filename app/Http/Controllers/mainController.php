2c46db631f0e3777652f22e5079c3ca2f9a054ea
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-05-09 19:47:41 +0700

    membuat controller dan routing index dengan controller

---
+++ app/Http/Controllers/mainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\DB;
+
+class mainController extends Controller
+{
+    public function index(){
+        $warga = DB::table('warga')->get();
+        return view('dashboard.index',['warga'=>$warga]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\mainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "Home";
-});
+Route::get('/',[mainController::class,'index']);
 
 
 
